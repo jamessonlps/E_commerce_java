8a0bff731b80f731a9fc41211f05644fe60b9bbe
ivanabb2 <62958684+ivanabb2@users.noreply.github.com>
2021-03-20 20:18:57 -0300

    Classe caixa + Testes

---
+++ src/br/edu/insper/desagil/aps/vaimalte/Caixa.java
@@ -0,0 +1,42 @@
+package br.edu.insper.desagil.aps.vaimalte;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Caixa {
+		
+	private Map<Integer, Integer> descontos;
+
+	public Caixa() 
+	{
+		this.descontos = new HashMap<>();
+	}
+	
+	public void incluiDesconto(Produto produto, int desconto) 
+	{
+		if ((desconto >= 1) && (desconto <= 99))
+		{
+			this.descontos.put(produto.getCodigo(), desconto);
+		}
+	}
+	
+	public double pedidoTotal(Carrinho carrinho)
+	{
+		double total = 0;
+		for (Pedido pedido : carrinho.getPedidos())
+		{
+			int codigo = pedido.getProduto().getCodigo();
+			if (descontos.containsKey(codigo))
+			{
+				double desconto = descontos.get(codigo);
+				total += pedido.valorTotal()*(1 - desconto/100);
+			}
+			else
+			{
+				total += pedido.valorTotal();
+			}
+		}
+		return total;
+	}
+	
+}
--- src/br/edu/insper/desagil/aps/vaimalte/Testador.java
+++ src/br/edu/insper/desagil/aps/vaimalte/Testador.java
@@ -3,18 +3,65 @@ package br.edu.insper.desagil.aps.vaimalte;
 public class Testador {
 	
 	public boolean testeA() {
+		Carrinho carrinho = new Carrinho();
+		
+		Caixa caixa = new Caixa();
+		
+		if (caixa.pedidoTotal(carrinho) == 0) {
+			return true;
+		}
+				
 	    return false;
 	}
 
 	public boolean testeB() {
+		Carrinho carrinho = new Carrinho();
+		
+		Produto guaranaJesus = new Produto(13, "Guarana Jesus", 4.65);
+		carrinho.verificaPedido(guaranaJesus);
+		
+		Caixa caixa = new Caixa();
+		
+		if (caixa.pedidoTotal(carrinho) == 4.65) {
+			return true;
+		}
+		
 	    return false;
 	}
 
 	public boolean testeC() {
+		Carrinho carrinho = new Carrinho();
+		
+		Produto guaranaJesus = new Produto(13, "Guarana Jesus", 4.65);
+		carrinho.verificaPedido(guaranaJesus);
+		
+		Caixa caixa = new Caixa();
+		caixa.incluiDesconto(guaranaJesus, 13);
+		
+		if (caixa.pedidoTotal(carrinho) == 4.0455) {
+			return true;
+		}
+		
 	    return false;
 	}
 
 	public boolean testeD() {
+		Carrinho carrinho = new Carrinho();
+		
+		Produto guaranaJesus = new Produto(13, "Guarana Jesus", 4.65);
+		Produto cuscuz = new Produto(06, "Cuscuz", 1.69);
+		
+		carrinho.verificaPedido(guaranaJesus);
+		carrinho.verificaPedido(guaranaJesus);
+		carrinho.verificaPedido(cuscuz);
+		
+		Caixa caixa = new Caixa();
+		caixa.incluiDesconto(cuscuz, 13);
+		
+		if (caixa.pedidoTotal(carrinho) == 2*4.65 + 1.69*(1-0.13)) {
+			return true;
+		}
+			    
 	    return false;
 	}
 
